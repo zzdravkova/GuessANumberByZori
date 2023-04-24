495cfba500c29b69db80b1f0f60ac472e43a0a35
Zornitsa Zdravkova <zornitsa.zdravkova92@gmail.com>
2023-04-24 17:36:03 +0300

    Uploaded my second git project

---
+++ GuessANumber.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessANumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Random randomNumber = new Random();
+        int computerNumber = randomNumber.nextInt(100);
+
+        while (true) {
+            System.out.println("Guess a number (1-100): ");
+            String playerInput = scanner.nextLine();
+
+            int playerNumber = 0;
+
+            boolean isValid = true;
+
+            for (int i = 0; i < playerInput.length(); i++) {
+                if (playerInput.charAt(i) < 48 || playerInput.charAt(i) > 57) {
+                    isValid = false;
+                    break;
+                }
+
+            }
+
+            if (isValid){
+                playerNumber = Integer.parseInt(playerInput);
+                if(playerNumber == computerNumber){
+                    System.out.println("You guessed it!");
+                } else if(playerNumber > computerNumber){
+                    System.out.println("Too High");
+                } else {
+                    System.out.println("Too Low");
+                }
+            } else {
+                System.out.println("Invalid input.");
+            }
+
+
+        }
+
+    }
+}
